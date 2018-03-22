f2dd8aaa5444671bd018eb14bc6ce0d627d29283
tb <t.bak@selleo.com>
2018-03-22 23:03:15 +0100

    Add fetchPosts API call

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import qs from 'query-string';
+
+export const api = axios.create({
+  baseURL: 'https://react-developers-workshops-server-einjjcgrqx.now.sh',
+});
+
+export const fetchPosts = ({limit, search}) =>
+  api.get(`/posts?${qs.stringify({_limit: limit, title_like: search})}`);
--- src/components/App/RouterPage.js
+++ src/components/App/RouterPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import qs from 'query-string';
 
 export default props => (
   <div>
     <h1>Router</h1>
-    <p>props</p>
+    <h4>search</h4>
+    <pre>{JSON.stringify(qs.parse(props.location.search), null, 2)}</pre>
+    <h4>props</h4>
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 );
