b754af353139bc11f7b6923731e2f24612397da0
tb <t.bak@selleo.com>
2019-07-16 13:38:33 +0200

    Extract fetch reducers

--- src/store/posts/reducer.js
+++ src/store/posts/reducer.js
@@ -1,7 +1,7 @@
-import { keyBy } from 'lodash';
 import produce from 'immer';
 
 import { FETCH_POSTS, FETCH_POST, UPDATE_POST } from './actionTypes';
+import { asyncFetchListReducer, asyncFetchReducer } from '../../utils'
 
 const initialState = {
   byId: {},
@@ -15,45 +15,11 @@ const initialState = {
 };
 
 export const reducer = produce((state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_POSTS.STARTED:
-      state.list.loading = true;
-      return;
-    case FETCH_POSTS.SUCCESS:
-      state.byId = {
-        ...state.byId,
-        ...keyBy(action.payload, 'id'),
-      };
-      state.list = {
-        ids: action.payload.map(({ id }) => id),
-        loading: false,
-        error: null,
-      };
-      return;
-    case FETCH_POSTS.FAILURE:
-      state.list = {
-        ids: [],
-        loading: false,
-        error: action.payload,
-      };
-      return;
-    case FETCH_POST.STARTED:
-    case UPDATE_POST.STARTED:
-      state.loadingById[action.payload.id] = true;
-      delete state.errorsById[action.payload.id];
-      return;
-    case FETCH_POST.SUCCESS:
-    case UPDATE_POST.SUCCESS:
-      state.loadingById[action.payload.id] = false;
-      state.byId[action.payload.id] = action.payload;
-      return;
-    case UPDATE_POST.FAILURE:
-      state.loadingById[action.payload.id] = false;
-      state.errorsById[action.payload.id] = action.payload.errors;
-      return;
-    default:
-      return state;
-  }
+  asyncFetchListReducer(FETCH_POSTS)(state, action);
+  asyncFetchReducer(FETCH_POST)(state, action);
+  asyncFetchReducer(UPDATE_POST)(state, action);
+
+  return state;
 });
 
 export default reducer;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+import { keyBy } from 'lodash';
+import produce from 'immer';
+
 export const createAsyncActionTypes = (actionName, resource) =>
   ['STARTED', 'FAILURE', 'SUCCESS'].reduce(
     (acc, status) => ({
@@ -30,3 +33,54 @@ export const createAsyncAction = (
       dispatch({ type: FAILURE, payload: { id: params.id, errors } });
     });
 };
+
+export const asyncFetchListReducer = ({ STARTED, SUCCESS, FAILURE }) =>
+  produce((state, action) => {
+    switch (action.type) {
+      case STARTED:
+        state.list.loading = true;
+        return;
+      case SUCCESS:
+        state.byId = {
+          ...state.byId,
+          ...keyBy(action.payload, 'id'),
+        };
+        state.list = {
+          ids: action.payload.map(({ id }) => id),
+          loading: false,
+          error: null,
+        };
+        return;
+      case FAILURE:
+        state.list = {
+          ids: [],
+          loading: false,
+          error: action.payload,
+        };
+        return;
+      default:
+        return;
+    }
+  });
+
+export const asyncFetchReducer = ({ STARTED, SUCCESS, FAILURE }) =>
+  produce((state, action) => {
+    switch (action.type) {
+      case STARTED:
+        state.loadingById[action.payload.id] = true;
+        delete state.errorsById[action.payload.id];
+        return;
+      case SUCCESS:
+        state.loadingById[action.payload.id] = false;
+        state.byId[action.payload.id] = action.payload;
+        return;
+      case FAILURE:
+        state.loadingById[action.payload.id] = false;
+        state.errorsById[action.payload.id] = action.payload.errors
+          ? action.payload.errors
+          : action.payload;
+        return;
+      default:
+        return;
+    }
+  });
